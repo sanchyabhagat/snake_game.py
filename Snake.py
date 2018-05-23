77d2ab18e3e246d2fa65360793fea730fc1a36d5
sanchyabhagat <sanchya.bhagat@gmail.com>
2018-05-22 19:15:43 -0700

    added snake and food vars

--- Snake.py
+++ Snake.py
@@ -14,6 +14,26 @@ screen_height, screen_width = screen.getmaxyx();
 #initialize a new window with the height and width
 window = curses.newwin(screen_height,screen_width,0,0)
 
+# accept keypad input
 window.keypad(1)
 window.timeout(100)
 
+# add snake body
+snake_x = screen_width / 4
+snake_y = screen_height / 2
+snake = [
+    [snake_y, snake_x],
+    [snake_y, snake_x-1],
+    [snake_y, snake_x-2]
+]
+
+print (str(snake_x) + ' ' + str(snake_y))
+
+food = [screen_height/2, screen_width/2]
+window.addch(food[0], food[1], curses.ACS_PI)
+#print (window)
+
+# initial direction of snake
+key = curses.KEY_RIGHT
+
+#Start game here
